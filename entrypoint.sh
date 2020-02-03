29f9b8bec2db48f86adb001c1491c983aaa9d306
FanhuaCloud <1157173310@qq.com>
2020-02-03 18:46:21 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
-#!/bin/ash
+#!/bin/bash
 cd /home/container
 
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+MODIFIED_STARTUP=$(echo $(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+START_COMMAND=$(echo -e ${MODIFIED_STARTUP})
+echo -e ":/home/container$ ${START_COMMAND}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+${MODIFIED_STARTUP}
